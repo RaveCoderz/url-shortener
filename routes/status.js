ba0cda20c367752247bb9d50b820c91024df7919
Yota002 <moscowrus2013@gmail.com>
2023-01-19 14:45:40 +0500

    Fix

--- routes/status.js
+++ routes/status.js
@@ -4,15 +4,15 @@ const axios = require("axios");
 
 let status = {};
 
-setInterval(() => {
-  axios
-    .post(
-      "https://api.uptimerobot.com/v2/getMonitors?api_key=ur1981401-a078c2eae5876ebd96f6b3c3&format=json"
-    )
-    .then((res) => {
-      status = res.data;
-    });
-}, 10000);
+// setInterval(() => {
+//   axios
+//     .post(
+//       "https://api.uptimerobot.com/v2/getMonitors?api_key=ur1981401-a078c2eae5876ebd96f6b3c3&format=json"
+//     )
+//     .then((res) => {
+//       status = res.data;
+//     });
+// }, 10000);
 
 router.get("/", async function (req, res, next) {
   res.sendStatus(200);
