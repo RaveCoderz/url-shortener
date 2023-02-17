b50b9b7324ab0606e974ae84c605a30bbb4ce340
Yota <moscowrus2013@gmail.com>
2023-02-17 11:56:42 +0500

    Revert "Deleted static"
    This reverts commit d3a9e11b905636b01ca386e8c2a3670f59b0f0ef.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const { initializeApp, cert } = require("firebase-admin/app");
@@ -20,6 +21,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 
